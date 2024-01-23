54b18ca64a53b10947ffb4108cdb92902813bd9e
Nicolas Barbosa <nicolasbarbosa1400@gmail.com>
2024-01-23 20:52:34 -0300

    exercicio de composição de classes

--- COMPOSIÇÃO/src/entites/HourContract.java
+++ COMPOSIÇÃO/src/entites/HourContract.java
@@ -1,5 +1,49 @@
 package entites;
 
+import java.util.Date;
+
 public class HourContract {
 
+	private Date date;
+	private Double valuorPerHour;
+	private Integer hours;
+	
+	public HourContract() {
+		
+	}
+
+	public HourContract(Date date, Double valuorPerHour, Integer hours) {
+		this.date = date;
+		this.valuorPerHour = valuorPerHour;
+		this.hours = hours;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getValuorPerHour() {
+		return valuorPerHour;
+	}
+
+	public void setValuorPerHour(Double valuorPerHour) {
+		this.valuorPerHour = valuorPerHour;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+	
+	public double totalValue() {
+	return valuorPerHour * hours; 
+	}
+	
 }
--- COMPOSIÇÃO/src/entites/Worker.java
+++ COMPOSIÇÃO/src/entites/Worker.java
@@ -1,5 +1,102 @@
 package entites;
 
-public class Worker {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
+import entites.enums.WorkerLevel;
+
+public class Worker {
+	
+	private WorkerLevel level;
+	private Double baseSalary;
+	private String name;
+	
+	private Department department;//associando com a classe "department" fazendo uma composição das classes
+	private List<HourContract> contracts = new ArrayList<>();//associando com a classe fazendo uma composição das classes(ex: um para varios)
+	
+	
+	public Worker() {
+		
+	}
+
+
+	public Worker(WorkerLevel level, Double baseSalary, String name, Department department) {
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.name = name;
+		this.department = department;
+	}//meu construtor não recebe uma lista(associação de "tem muitos") e eu instancio ela no arraylist
+	
+	
+	 
+	
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Department getDepartment() {
+		return department;
+	}
+
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for(HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year= cal.get(Calendar.YEAR);
+			int c_month = 1+ cal.get(Calendar.MONTH);
+			if(year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+			
+		}
+		return sum; 
+		
+	}
+	
 }
